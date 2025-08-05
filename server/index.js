a16b59590ce9b95bbb58e6c6b18f21abe2aeeda4
Harshini2410 <npoornimahasrhini2003@gmail.com>
2025-08-05 19:19:05 +0530

    Initial commit for Round 1 Task

---
+++ server/index.js
@@ -0,0 +1,19 @@
+// server/index.js
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/api/data', (req, res) => {
+  res.json({
+    name: 'Poornima Maharshini',
+    referralCode: 'poornima2025',
+    totalRaised: 1200, // dummy amount
+  });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
